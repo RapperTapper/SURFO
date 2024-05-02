8408309760b3e685531c6d9e5eeea8885a3d27e6
RapperTapper <mail@raphaelschnell.ch>
2024-05-02 09:30:04 +0200

    update extract.php

--- extract.php
+++ extract.php
@@ -10,29 +10,28 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 $output = curl_exec($ch);
 
-echo $output;
+// echo $output;
 
 // Speichere alle Daten in Variablen
 $data = json_decode($output, true); // decode the JSON feed
 
 // make new arry with needed information
 $aareguru_data = [];
-foreach ($data as $item) {
-    $latitude = $item['latitude']; // get the latitude
-    $longitude = $item['longitude']; // get the longitude
-    $temperature_2m = $item['current']['temperature_2m']; // get the temperature_2m
-    $precipitation = $item['current']['precipitation']; // get the precipitation
-    $cloud_cover = $item['current']['cloud_cover']; // get the cloud_cover
 
-    $aareguru_data[] = [
-        'latitude' => $latitude,
-        'longitude' => $longitude,
-        'temperature_2m' => $temperature_2m,
-        'precipitation' => $precipitation,
-        'cloud_cover' => $cloud_cover
-    ];
-}
+$thun_data = $data['values']['thun']; // get the data for Thun
+
+$temperature = $thun_data['temperature']; // get the temperature
+$flow = $thun_data['flow']; // get the flow
+$forecast2h = $thun_data['forecast2h']; // get the forecast2h
+$tt = $thun_data['tt']; // get the tt
+
+$aareguru_data[] = [
+    'temperatur' => $temperature,
+    'wasserfluss' => $flow,
+    'vorhersage2h' => $forecast2h,
+    'lufttemperatur' => $tt
+];
 
 // print_r($aareguru_data);
 
-// echo $aareguru_data[0]['latitude'];
+// echo $aareguru_data[0]['wasserfluss'];
