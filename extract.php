e03330b4768815e6142c849725219fa288476461
RapperTapper <mail@raphaelschnell.ch>
2024-05-02 10:29:14 +0200

    update with comments extract.php

--- extract.php
+++ extract.php
@@ -1,5 +1,5 @@
 <?php
-
+// 2 Fragen in den Kommentaren für Coaching
 $url = "https://aareguru.existenz.ch/v2018/widget?app=surfo.app.ch&version=1.0.42";
 
 // curl
@@ -18,8 +18,11 @@ $data = json_decode($output, true); // decode the JSON feed
 // make new arry with needed information
 $aareguru_data = [];
 
+// was passiert, wenn wir keinen Wert zurück erhalten für thun?
 $thun_data = $data['values']['thun']; // get the data for Thun
 
+// wir sammeln keine Ortsdaten, sondern nur die Werte. Falls zu einem späteren Zeitpunk ein weiterer Ort hinzugefügt wird, muss dieser Code hier ergänzt werden.
+// Wie würden wir hier die Daten kontrollieren, ob sie vorhanden sind bzw. in der richtigen Form vorliegen?
 $temperature = $thun_data['temperature']; // get the temperature
 $flow = $thun_data['flow']; // get the flow
 $forecast2h = $thun_data['forecast2h']; // get the forecast2h
