dda75d8641aa44ef01b7dff98f9ca56d26534590
juliaannamiller <julia.miller@stud.fhgr.ch>
2024-05-02 10:28:41 +0200

    update transform.php

--- transform.php
+++ transform.php
@@ -2,28 +2,39 @@
 
 include 'extract.php';
 
-// echo "Hello Transoform Raphi!";
-
-// print_r($weather_data);
-
-// make map with lat / lon to location
-$locations = [
-    '46.94,7.44' => 'Bern',
-    '46.84,9.52' => 'Chur'
-];
+// print_r($aareguru_data);
 
 // new function weather_condition
-function weather_condition( $precipitation, $cloud_cover) {
-    // cloud_cover: 0-20 = sunny, 21-80 = cloudy
-    if ($cloud_cover <= 80 && $precipitation == 0) {
-        return 'sunny';
-    } elseif ($cloud_cover > 80 && $precipitation < 5) {
-        return 'cloudy';
-    } elseif ($precipitation >= 5) {
-        return 'rainy';
-    } else {
-        return 'unknown';
-    }
+function surf_condition($flow, $temperature) {
+
+    // temperature: iisig<10; 10<=erfrüschend<17; 17<=agnähm<21; 21<=warm<25; z'warm>=25
+    // flow: keni<100; 100<=gueti<200; 200<=grossi<300; z'mächtigi>=300   
+    // return "Ds Wasser isch <condition>, u Wäue hets <condition>!"  
+    // if $flow or $temperature is not a number return "Hüt gits kei Spruch :(" 
+    $temp_adj = '';
+    $wasser_adj = '';
+    if (!is_numeric($flow) || !is_numeric($temperature)) {
+        return "Hüt gits kei Spruch :(";
+    } elseif ($temperature < 10) {
+        $temp_adj = 'iisig';
+    } elseif ($temperature >= 10 && $temperature < 17) {
+        $temp_adj = 'erfrüschend';
+    } elseif ($temperature >= 17 && $temperature < 21) {
+        $temp_adj = 'agnähm';
+    } elseif ($temperature >= 21 && $temperature < 25) {
+        $temp_adj = 'warm';
+    } elseif ($temperature >= 25) {
+        $temp_adj = 'z\'warm';
+    } elseif ($flow < 100) {
+        $wasser_adj = 'keni';
+    } elseif ($flow >= 100 && $flow < 200) {
+        $wasser_adj = 'gueti';
+    } elseif ($flow >= 200 && $flow < 300) {
+        $wasser_adj = 'grossi';
+    } elseif ($flow >= 300) {
+        $wasser_adj = 'z\'mächtigi';
+    }       
+    return "Ds Wasser isch " . $temp_adj . ", u Wäue hets " . $wasser_adj . "!";
 }
 
 // transform data
