995f24a0ff49392e7de664d7bb13924bb0df7a8a
juliaannamiller <julia.miller@stud.fhgr.ch>
2024-05-02 11:22:45 +0200

    update transform.php

--- transform.php
+++ transform.php
@@ -4,7 +4,11 @@ include 'extract.php';
 
 // print_r($aareguru_data);
 
-// new function weather_condition
+$surfCondition = surf_condition($flow, $temperature);
+
+echo $surfCondition;
+
+// new function surf_condition
 function surf_condition($flow, $temperature) {
 
     // temperature: iisig<10; 10<=erfrüschend<17; 17<=agnähm<21; 21<=warm<25; z'warm>=25
@@ -41,29 +45,3 @@ function surf_condition($flow, $temperature) {
     return "Ds Wasser isch " . $temp_adj . ", u Wäue hets " . $wasser_adj . "!";
 }
 
-// reihenfolge vom code?
-$surfCondition = surf_condition($flow, $temperature);
-
-echo $surfCondition;
-
-// transform data
-foreach ($weather_data as $index => $item) {
-
-    // round temperature to integer
-    $weather_data[$index]['temperature_2m'] = round($item['temperature_2m']);
-
-    // convert latitude and longitude to location
-    $coordinates = $item['latitude'] . ',' . $item['longitude'];
-    // echo "Coordinates: " . $coordinates . "<br>";
-    
-    // add location to weather data
-    $weather_data[$index]['location'] = $locations[$coordinates];
-
-    // remove latitude and longitude
-    unset($weather_data[$index]['latitude']);
-    unset($weather_data[$index]['longitude']);
-
-    $weather_data[$index]['condition'] = weather_condition($item['precipitation'], $item['cloud_cover']);
-}
-
-print_r($weather_data);
