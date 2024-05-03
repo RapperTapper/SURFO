23f0a8338b53fc131e006c3935b113128027d978
juliaannamiller <julia.miller@stud.fhgr.ch>
2024-05-03 12:26:37 +0200

    endpoint.php unixtime hinzu

--- endpoint.php
+++ endpoint.php
@@ -8,12 +8,12 @@ try {
     $wasserfluss = [];
     $temperatur = [];
     $lufttemperatur = [];
-    // $unixtime = [];
+    $unixtime = [];
 
     $pdo = new PDO($dsn, $db_user, $db_pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
-    $query = "SELECT wasserfluss, temperatur, lufttemperatur FROM Surfo";
+    $query = "SELECT wasserfluss, temperatur, lufttemperatur, unixtime FROM Surfo";
     $stmt = $pdo->prepare($query);
     $stmt->execute();
     
@@ -24,21 +24,24 @@ try {
         $wasserfluss[] = $row['wasserfluss'];
         $temperatur[] = $row['temperatur'];
         $lufttemperatur[] = $row['lufttemperatur'];
-        // $unixtime = $row['unixtime'];
+        $unixtime[] = $row['unixtime'];
         
     }
 
     // print_r($wasserfluss);
     // print_r($temperatur);
     // print_r($lufttemperatur);
+    // print_r($unixtime);
 
     $wasserflussJson = json_encode(array_map('floatval', $wasserfluss));
     $temperaturJson = json_encode(array_map('floatval', $temperatur));
     $lufttemperaturJson = json_encode(array_map('floatval', $lufttemperatur));
+    $unixtimeJson = json_encode(array_map('floatval', $unixtime));
 
     echo $wasserflussJson;
     echo $temperaturJson;
     echo $lufttemperaturJson;
+    echo $unixtimeJson;
 
 } catch (PDOException $e) {
     die("ERROR: Could not able to execute $query. " . $e->getMessage());
