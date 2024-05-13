1a62b54234557b0af0d9c51f3c17e43cb2877f81
RapperTapper <mail@raphaelschnell.ch>
2024-05-13 16:00:00 +0200

    update endpoint so that json is valid with an object inside

--- endpoint.php
+++ endpoint.php
@@ -27,21 +27,63 @@ try {
         $unixtime[] = $row['unixtime'];
         
     }
+    // Code V02
+    $data = [
+        'wasserfluss' => array_map('floatval', $wasserfluss),
+        'temperatur' => array_map('floatval', $temperatur),
+        'lufttemperatur' => array_map('floatval', $lufttemperatur),
+        'unixtime' => array_map('floatval', $unixtime),
+    ];
+    // Code V02
+    $allData = json_encode($data);
 
     // print_r($wasserfluss);
     // print_r($temperatur);
     // print_r($lufttemperatur);
     // print_r($unixtime);
 
-    $wasserflussJson = json_encode(array_map('floatval', $wasserfluss));
-    $temperaturJson = json_encode(array_map('floatval', $temperatur));
-    $lufttemperaturJson = json_encode(array_map('floatval', $lufttemperatur));
-    $unixtimeJson = json_encode(array_map('floatval', $unixtime));
+    // Code V01
+    // $wasserflussJson = json_encode(array_map('floatval', $wasserfluss));
+    // $temperaturJson = json_encode(array_map('floatval', $temperatur));
+    // $lufttemperaturJson = json_encode(array_map('floatval', $lufttemperatur));
+    // $unixtimeJson = json_encode(array_map('floatval', $unixtime));
+    
+    // Code V01
+    // echo $wasserflussJson;
+    // echo $temperaturJson;
+    // echo $lufttemperaturJson;
+    // echo $unixtimeJson;
 
-    echo $wasserflussJson;
-    echo $temperaturJson;
-    echo $lufttemperaturJson;
-    echo $unixtimeJson;
+    // Code V02
+    echo $allData;
+
+    // Code to print the latest data - just for testing
+    // echo "<br>";
+    // echo "<br>";
+    // echo "Data successfully retrieved";
+    // echo "<br>";
+    // echo "Latest wasserfluss: " . end($data['wasserfluss']);
+    // echo "<br>";
+    // echo "Latest temperatur: " . end($data['temperatur']);
+    // echo "<br>";
+    // echo "Latest lufttemperatur: " . end($data['lufttemperatur']);
+    // echo "<br>";
+    // echo "Latest unixtime: " . end($data['unixtime']);
+    // echo "<br>";
+
+    // Code to print the latest data from my json object - just for testing
+    $decodedData = json_decode($allData, true);
+    
+    echo "<br>";
+    echo "<br>";
+    echo "Latest wasserfluss: " . end($decodedData['wasserfluss']);
+    echo "<br>";
+    echo "Latest temperatur: " . end($decodedData['temperatur']);
+    echo "<br>";
+    echo "Latest lufttemperatur: " . end($decodedData['lufttemperatur']);
+    echo "<br>";
+    echo "Latest unixtime: " . end($decodedData['unixtime']);
+    echo "<br>";
 
 } catch (PDOException $e) {
     die("ERROR: Could not able to execute $query. " . $e->getMessage());
