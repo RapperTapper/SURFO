9cadb14514433fe01a0798e50f2b90f665d6612a
viluronio <119875798+viluronio@users.noreply.github.com>
2024-05-14 16:40:02 +0200

    jan hackt den hacker aus dem hacksalat rhabarberkuchen #yeah

--- script-jan.js
+++ script-jan.js
@@ -2,73 +2,101 @@ console.log("Hello World!");
 
 async function fetchData() {
     try {
-        const response = await fetch ('https://732516-12.web.fhgr.ch/endpoint.php');
+        const response = await fetch('https://732516-12.web.fhgr.ch/endpoint.php');
         const data = await response.json();
-        //console.log(data);
         return data;
     } catch (error) {
         console.error(error);
     }
 }
 
-//asynchrone funktion mit await überprüfen 
-async function main () {
+async function main() {
     let data = await fetchData();
     let lufttemperatur = data.data.lufttemperatur;
     let temperatur = data.data.temperatur;
     let unixtime = data.data.unixtime;
     let wasserfluss = data.data.wasserfluss;
 
-    // console.log(`Lufttemperatur: ${lufttemperatur}`);
-    // console.log(`Temperatur: ${temperatur}`);
-     console.log(unixtime);
-    // console.log(`Wasserfluss: ${wasserfluss}`);
+    console.log(unixtime);
 
-    // Get the current time in seconds
     const currentTime = Math.floor(Date.now() / 1000);
-
-    // Calculate the timestamp for 24 hours ago
     const twentyFourHoursAgo = currentTime - (24 * 60 * 60);
-
-    // Filter the unixtime array to get the timestamps within the last 24 hours
     const latestTwentyFourHours = unixtime.filter(timestamp => timestamp >= twentyFourHoursAgo);
-
-    // Filter the wasserfluss array to get the values within the last 24 hours
     const latestWasserfluss = wasserfluss.filter((_, index) => unixtime[index] >= twentyFourHoursAgo);
 
-     
-
-    // Create a line chart
     const ctx = document.getElementById('wasserflussChart').getContext('2d');
+
     const myChart = new Chart(ctx, {
         type: 'line',
         data: {
-            labels: latestTwentyFourHours.map(timestamp => new Date(timestamp * 1000).toLocaleTimeString('de-ch', {hour: '2-digit', minute: '2-digit'})),
+            labels: latestTwentyFourHours.map(timestamp => new Date(timestamp * 1000).toLocaleTimeString('de-ch', {
+                hour: '2-digit',
+                minute: '2-digit'
+            })),
             datasets: [{
                 label: 'Wasserfluss',
-                data: wasserfluss,
-                backgroundColor: 'rgba(0, 123, 255, 0.2)',
-                borderColor: 'rgba(0, 123, 255, 1)',
-                borderWidth: 1
+                data: latestWasserfluss,
+                backgroundColor: '#FFFFFF',
+                borderColor: '#FFFFFF',
+                borderWidth: 4
             }]
         },
+        
         options: {
+
             scales: {
                 x: {
                     title: {
-                        display: true,
+                        display: false,
                         text: 'Zeit'
+                    },
+                    ticks: {  // Hier werden die Werte der X-Achse angepasst
+                        display: true,
+                        color: '#FFFFFF',  // Farbe der X-Achsen-Werte
+                        font: {
+                            family: 'Avenir',  // Schriftart der X-Achsen-Werte
+                            size: 14,          // Schriftgröße der X-Achsen-Werte
+                            weight: 'bold'     // Schriftgewicht der X-Achsen-Werte
+                        }
                     }
                 },
                 y: {
                     title: {
-                        display: true,
-                        text: 'Wasserfluss in m3'
+                        display: false,
+                        text: 'Wasserfluss in m³'
+                    },
+                    ticks: {  // Hier werden die Werte der Y-Achse angepasst
+                        color: '#FFFFFF',  // Farbe der Y-Achsen-Werte
+                        font: {
+                            family: 'Avenir',  // Schriftart der Y-Achsen-Werte
+                            size: 14,          // Schriftgröße der Y-Achsen-Werte
+                            weight: 'Avenir LT Pro 750'     // Schriftgewicht der Y-Achsen-Werte
+                        }
+                    },
+                    grid: {
+                        color: '#374577',  // Farbe der horizontalen Linien
+                        lineWidth: 3       // Dicke der horizontalen Linien
                     }
                 }
+            },
+            plugins: {
+                legend: {
+                    display: false
+                },
+                tooltip: {
+                    enabled: false
+                },
+                beforeDraw: (chart) => {
+                    const ctx = chart.ctx;
+                    ctx.save();
+                    ctx.fillStyle = '#000000'; // Hintergrundfarbe
+                    ctx.fillRect(0, 0, chart.width, chart.height);
+                    ctx.restore();
+                }
+                
             }
         }
     });
 }
-   
-main ();
+
+main();
