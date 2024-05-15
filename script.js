a6d4a2b5a975e28000f73cde4c2a83db6cdb29d4
juliaannamiller <julia.miller@stud.fhgr.ch>
2024-05-15 11:16:01 +0200

    script.js tabelle styling

--- script.js
+++ script.js
@@ -40,8 +40,11 @@ async function main () {
     let times = latestTwentyFourHours.map(timestamp => new Date(timestamp * 1000).toLocaleTimeString('de-ch', {hour: '2-digit', minute: '2-digit', hour12: false}))
     
     const level = latestWasserfluss.flat();
-    const unten = Math.min(...level) - 30;
+    const unten = Math.min(...level) - 15;
     const oben = Math.max(...level) + 5;
+
+    // console.log(unten);
+    // console.log(oben);
 
     // Create a line chart
     const ctx = document.getElementById('wasserflussChart').getContext('2d');
@@ -89,8 +92,8 @@ async function main () {
                     // }
                 },
                 y: {
-                    // min: 50, // Set the minimum value of y-axis to 50
-                    // // max: 200, // Set the maximum value of y-axis to 200
+                     min: unten, // Set the minimum value of y-axis to 50
+                     max: oben, // Set the maximum value of y-axis to 200
                     title: {
                         display: true,
                         text: 'Wasserfluss in m3/s',
