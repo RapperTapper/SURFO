c28dae3040aeeef853ca34b0e11a66140225e935
RapperTapper <mail@raphaelschnell.ch>
2024-05-15 18:13:15 +0200

    some changes

---
+++ switch-space.js
@@ -0,0 +1,10 @@
+// Get the div element with the id "erweitert"
+const erweitertDiv = document.getElementById("erweitert");
+
+// Add event listener to the div
+erweitertDiv.addEventListener("click", myFunction);
+
+// Function to be called when the div is clicked
+function myFunction() {
+    console.log("Div clicked!");
+}
