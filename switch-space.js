9006e10886546dc4881950c273117b7caf73a972
RapperTapper <mail@raphaelschnell.ch>
2024-05-23 01:11:09 +0200

    added funtion to btns

--- switch-space.js
+++ switch-space.js
@@ -2,6 +2,7 @@
 const erweitertDivs = document.querySelectorAll(".erweitert");
 const zurueckDivs = document.querySelectorAll(".zurueck");
 
+let infoDivPosition = 0;
 // Add event listener to the div
 erweitertDivs.forEach(div => {
     div.addEventListener("click", switchToObserver);
@@ -13,8 +14,32 @@ zurueckDivs.forEach(div => {
 // Function to be called when the div is clicked
 function switchToObserver() {
     console.log("Div clicked!");
+    
+    // Get all the elements with the class "info-div"
+    const infoDivs = document.querySelectorAll(".info-div");
+
+    // Loop through the elements and apply the CSS rules
+    infoDivs.forEach(div => {
+        div.style.left = "0%";
+        div.style.width = "100%";
+        div.style.maxWidth = "100%";
+    });
+
+    infoDivPosition = 1;
 }
 
 function switchToSurfo() {
     console.log("Div clicked!");
+
+    // Get all the elements with the class "info-div"
+    const infoDivs = document.querySelectorAll(".info-div");
+
+    // Loop through the elements and apply the CSS rules
+    infoDivs.forEach(div => {
+        div.style.left = "110%";
+        div.style.width = "100%";
+        div.style.maxWidth = "100%";
+    });
+
+    infoDivPosition = 0;
 }
