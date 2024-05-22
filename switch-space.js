30ab478ec606488d241c600e6604bab0e4970592
RapperTapper <mail@raphaelschnell.ch>
2024-05-23 01:56:44 +0200

    all btns work as they should

--- switch-space.js
+++ switch-space.js
@@ -17,12 +17,17 @@ function switchToObserver() {
     
     // Get all the elements with the class "info-div"
     const infoDivs = document.querySelectorAll(".info-div");
+    const liveInfoContainerZurueck = document.querySelectorAll(".live-info-container-zurueck");
 
     // Loop through the elements and apply the CSS rules
     infoDivs.forEach(div => {
         div.style.left = "0%";
         div.style.width = "100%";
         div.style.maxWidth = "100%";
+    });
+
+    liveInfoContainerZurueck.forEach(div => {
+        div.style.left = "-395px";
     });
 
     infoDivPosition = 1;
@@ -33,6 +38,7 @@ function switchToSurfo() {
 
     // Get all the elements with the class "info-div"
     const infoDivs = document.querySelectorAll(".info-div");
+    // const liveInfoContainerZurueck = document.querySelectorAll(".live-info-container-zurueck");
 
     // Loop through the elements and apply the CSS rules
     infoDivs.forEach(div => {
@@ -40,6 +46,93 @@ function switchToSurfo() {
         div.style.width = "100%";
         div.style.maxWidth = "100%";
     });
+    // liveInfoContainerZurueck.forEach(div => {
+    //     div.style.left = "-800px";
+    // });
 
     infoDivPosition = 0;
 }
+
+function applyStylesBasedOnAspectRatio() {
+    // Get the window's aspect ratio
+    const aspectRatio = window.innerWidth / window.innerHeight;
+
+    const infoDivs = document.querySelectorAll(".info-div");
+    const surferDivs = document.querySelectorAll(".surfer-div");
+    const liveInfoContainer = document.querySelectorAll(".live-info-container");
+    const liveInfoContainerZurueck = document.querySelectorAll(".live-info-container-zurueck");
+
+    // Check the aspect ratio and the value of infoDivPosition
+    if (aspectRatio <= 0.73) {
+        if (infoDivPosition == 1) {
+            infoDivs.forEach(div => {
+                div.style.left = "0%";
+                div.style.width = "100%";
+                div.style.maxWidth = "100%";
+            });
+            surferDivs.forEach(div => {
+                div.style.left = "0%";
+                div.style.width = "100%";
+                div.style.maxWidth = "100%";
+            });
+            liveInfoContainer.forEach(div => {
+                div.style.right = "-295px";
+            });
+            liveInfoContainerZurueck.forEach(div => {
+                div.style.left = "-395px";
+            });
+        } else if (infoDivPosition == 0) {
+            infoDivs.forEach(div => {
+                div.style.left = "110%";
+                div.style.width = "100%";
+                div.style.maxWidth = "100%";
+            });
+            surferDivs.forEach(div => {
+                div.style.left = "0%";
+                div.style.width = "100%";
+                div.style.maxWidth = "100%";
+            });
+            liveInfoContainer.forEach(div => {
+                div.style.right = "-295px";
+            });
+            liveInfoContainerZurueck.forEach(div => {
+                div.style.left = "-800px";
+            });
+        } 
+    } else if (aspectRatio <= 1/0.68) {
+        surferDivs.forEach(div => {
+            div.style.width = "50%";
+            div.style.maxWidth = "50%";
+        });
+        infoDivs.forEach(div => {
+            div.style.left = "50%";
+            div.style.width = "50%";
+            div.style.maxWidth = "50%";
+        });
+        liveInfoContainerZurueck.forEach(div => {
+            div.style.left = "-800px";
+        });
+        liveInfoContainer.forEach(div => {
+            div.style.right = "-800px";
+        });
+    } else {
+        surferDivs.forEach(div => {
+            div.style.width = "30%";
+            div.style.maxWidth = "30%";
+        });
+        infoDivs.forEach(div => {
+            div.style.left = "30%";
+            div.style.width = "70%";
+            div.style.maxWidth = "70%";
+        });
+        liveInfoContainerZurueck.forEach(div => {
+            div.style.left = "-800px";
+        });
+        liveInfoContainer.forEach(div => {
+            div.style.right = "-800px";
+        });
+    }
+};
+
+window.onload = applyStylesBasedOnAspectRatio;
+window.onresize = applyStylesBasedOnAspectRatio;
